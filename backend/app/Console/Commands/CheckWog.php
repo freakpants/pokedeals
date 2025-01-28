fcacd8813ff1affeb73430380f664dd4f6f93296
freakpants <freakpants@gmail.com>
2025-01-28 08:10:41 +0100

    new data

--- backend/app/Console/Commands/CheckWog.php
+++ backend/app/Console/Commands/CheckWog.php
@@ -54,7 +54,7 @@ class CheckWog extends Command
             // echo the amount of data['products']
             $productCount = count($data['products']);
             echo $productCount;
-            $previousCount = 119;
+            $previousCount = 116;
             if($productCount === $previousCount){
                 // $domail = true;
                 $subject = "WOG: Product Count is " . $previousCount;
